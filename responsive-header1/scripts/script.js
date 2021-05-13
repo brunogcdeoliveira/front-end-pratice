775078f2f5173a4dc3a44e1fb5769c4d47dede04
Bruno de Oliveira <43137306+brunogcdeoliveira@users.noreply.github.com>
2021-05-13 17:29:24 -0300

    responsive header1 menu toggle updated

--- responsive-header1/scripts/script.js
+++ responsive-header1/scripts/script.js
@@ -1,6 +1,7 @@
 const $header = document.querySelector('header');
 const $logo = document.querySelectorAll('.logo')[0];
 const $navBar = document.querySelectorAll('.nav-bar')[0];
+const $menu = document.querySelectorAll('.menu')[0];
 
 window.addEventListener('scroll', toggleHeader, false);
 function toggleHeader() {
@@ -12,6 +13,8 @@ function toggleHeader() {
         $logo.classList.add('min-logo');
         $navBar.classList.remove('max-nav');
         $navBar.classList.add('min-nav');
+        $menu.firstElementChild.classList.remove('max-hamburger');
+        $menu.firstElementChild.classList.add('min-hamburger');
     } else if (window.pageYOffset <= 60 && $header.classList.contains('min-header')) {
         $header.classList.add('max-header');
         $header.classList.remove('min-header');
@@ -20,5 +23,25 @@ function toggleHeader() {
         $logo.classList.remove('min-logo');
         $navBar.classList.add('max-nav');
         $navBar.classList.remove('min-nav');
+        $menu.firstElementChild.classList.add('max-hamburger');
+        $menu.firstElementChild.classList.remove('min-hamburger');
+    }
+}
+
+$menu.addEventListener('click', toggleMenu, false);
+var isOpen = false;
+function toggleMenu() {
+    if (!isOpen) {
+        $navBar.classList.add('menu-opened');
+        isOpen = true;
+    } else {
+        $navBar.classList.remove('menu-opened');
+        isOpen = false;
+    }
+}
+$navBar.addEventListener('click', navClick, false);
+function navClick(evt) {
+    if (evt.target.tagName == 'A') {
+        toggleMenu();
     }
 }
